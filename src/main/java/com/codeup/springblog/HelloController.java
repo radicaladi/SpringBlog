608ef118d337188b4a6dd922abdbe16f89d78a57
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-18 12:35:53 -0700

    initial setup, add hello controller

---
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -0,0 +1,21 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello from Spring! Be prepared for some epic coding with the framework!";
+    }
+
+    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+    @ResponseBody
+    public String addOne(@PathVariable int number) {
+        return number + " plus one is " + (number + 1) + "!";
+    }
+
+}
