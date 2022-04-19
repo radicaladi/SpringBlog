2595be4183e15a4a5cd75f171d37be065eeabafa
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-19 08:49:59 -0700

    add passing data to view

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,21 +1,23 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 class HelloController {
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String hello() {
-        return "Hello from Spring! Be prepared for some epic coding with the framework!";
+    @GetMapping("/index/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("NameInput", name);
+        return "index";
     }
 
-    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
-    @ResponseBody
-    public String addOne(@PathVariable int number) {
-        return number + " plus one is " + (number + 1) + "!";
-    }
+
+//    @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
+//    @ResponseBody
+//    public String addOne(@PathVariable int number) {
+//        return number + " plus one is " + (number + 1) + "!";
+//    }
 
 }
