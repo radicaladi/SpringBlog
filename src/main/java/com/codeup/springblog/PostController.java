76d9d4b07f95ef0ca6759ffe5f7adee70403756f
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-19 17:55:48 -0700

    refactor for post class  entity

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,6 +1,6 @@
 package com.codeup.springblog;
 
-import model.Post;
+import com.codeup.springblog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
