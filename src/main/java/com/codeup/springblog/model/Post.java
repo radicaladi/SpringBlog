db785c971a184e6603b3e1ab48c39a0d8ae6cd4f
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-19 17:55:09 -0700

    minor change

---
+++ src/main/java/com/codeup/springblog/model/Post.java
@@ -0,0 +1,30 @@
+package com.codeup.springblog;
+
+import javax.persistence.*;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 100)
+    private String title;
+    @Column(nullable = false)
+    private String body;
+
+    public Post( String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post() {}
+
+    public String getTitle() { return title; }
+
+    public void setTitle(String title) { this.title = title; }
+
+    public String getBody() { return body; }
+
+    public void setBody(String body) { this.body = body; }
+}
