e573d15c2be4d38c13523bd4fe15802a500b9814
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-21 07:44:02 -0700

    refactor post; add id method

--- src/main/java/com/codeup/springblog/model/Post.java
+++ src/main/java/com/codeup/springblog/model/Post.java
@@ -8,18 +8,23 @@ public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
     @Column(nullable = false, length = 100)
     private String title;
     @Column(nullable = false)
     private String body;
 
-    public Post( String title, String body) {
+    public Post(long id, String title, String body) {
+        this.id = id;
         this.title = title;
         this.body = body;
     }
 
     public Post() {}
+
+    public long getId() { return id; }
+
+    public void setId(long id) { this.id = id; }
 
     public String getTitle() { return title; }
 
