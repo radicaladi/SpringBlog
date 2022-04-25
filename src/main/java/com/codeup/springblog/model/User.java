4f623f253152f71cdb9746e3c062e52006029a2c
Adrian Brown <adrian.r.brown90@gmail.com>
2022-04-25 12:19:21 -0700

    add user copy constructor

--- src/main/java/com/codeup/springblog/model/User.java
+++ src/main/java/com/codeup/springblog/model/User.java
@@ -25,6 +25,13 @@ public class User {
 
     public User() {}
 
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
     public User(String username, String email, String password, List<Post> posts) {
         this.username = username;
         this.email = email;
